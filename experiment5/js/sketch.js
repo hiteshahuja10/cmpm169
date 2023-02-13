2aea5c1606fff7659b3e917fefbccf54605a4dbf
“Hitesh <hahuja@ucsc.edu>
2023-02-13 15:13:40 -0800

    add sphere, color, transformations

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,67 +1,78 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Hitesh Ahuja, Johnny Wong
+// Date: 2/11/23
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+let angle = 0;
+let size = 100;
 
-// Globals
-let myInstance;
-let canvasContainer;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+	createCanvas(windowWidth, windowHeight,WEBGL);
+	// createEasyCam();
+    lights();
+	background(100);
+    red = random(0,255);
+    green = random(0,255);
+    blue = random(0,255);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    let movingX = windowWidth/6;
+    let movingY = windowHeight/6;
+	
+    rotateZ(angle);
+    rotateX(angle);
+    rotateY(angle);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+    let time_ct = frameCount/60;
+    if(time_ct % 5 == 0){
+        red = random(0,255);
+        green = random(0,255);
+        blue = random(0,255);
+    }
+    // noStroke();
+    red += 255/size/5;
+    green += 255/size/5;
+    blue += 255/size/5;
+
+    fill(red, green, blue);
+	sphere(size);
+    noFill("white");
+	box(size*2);
+
+
+    rotateX(sin(angle/3)+ (random(2)/10));
+    rotateY(cos(angle/2)+ (random(2)/10));
+	push();
+	translate(movingX,movingY);
+	fill('red');
+	box(10);
+	pop();
+
+    rotateX(cos(angle/3)+ (random(2)/10));
+    rotateY(sin(angle/2)+ (random(2)/10));
+    push();
+	translate(-movingX,-movingY);
+	fill('red');
+	box(10);
+	pop();
+    //angle += 0.5;
+
+    movingX += sin(angle)*1000;
+	angle += 0.05;
+    if (size > 400){
+        red = random(0,255);
+        green = random(0,255);
+        blue = random(0,255);
+        size = 100;
+    }
+    else{
+        size += 0.5;
+    } 
 }
